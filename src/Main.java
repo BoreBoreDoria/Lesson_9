e9ef57283eb7270214bcefe94c555c2892d51e1c
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-26 21:48:29 +0300

    Пример try with resources на сосбтвенном ресурсе

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,13 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String out = "Enter the next\n"; // Данная реализацяи работает только на цифрах,
-        // сиволах и англиских буквах
+    public static void main(String[] args){
 
-        try (OutputStream printStream = System.out) { // try with resources .
-            // Данная конструкция позволяет нам создавать ресурс, пользоваться им.
-            // А закрытие произойдёт автоматически.
-            // Ресурсом может быть любой класс, который унаследовал интерфейс Closeable
-            // Под капотом создаётся блок finally, где вызывается метод close()
-
-            write(out, printStream);
-
-            byte[] info = new byte[12];
-            System.in.read(info);
-
-            write("You entered: ", printStream);
-
-            for (int i = 0; i < info.length; i++) {
-                System.out.write(info[i]);
-            }
-
+        try (TestStream testStream = new TestStream()) { //Открыли ресурс
+            System.out.println("Выполняем запись данных"); // Поработали с ресурсом
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        OutputStream printStream = System.out;
-        try {
-            write(out, printStream);
-
-            byte[] info = new byte[12];
-            System.in.read(info);
-
-            write("You entered: ", printStream);
-
-            for (int i = 0; i < info.length; i++) {
-                System.out.write(info[i]);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            printStream.close();
-        }
-    }
-
-    private static void write(String out, OutputStream printStream) throws IOException {
-        char[] mas = out.toCharArray();
-
-        for (int i = 0; i < out.length(); i++) {
-            int a = mas[i];
-            printStream.write(a);
-        }
-        printStream.flush();
+        // После чего автоматически его закрыли
     }
 }
