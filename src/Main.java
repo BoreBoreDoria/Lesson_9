2f8b9fbc43a9fd7e4338a728509267a1f7d71319
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-26 21:30:50 +0300

    Пример реализация вводы и вывода данных в консоль через InputStream и OutputStream

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String out = "Enter the next\n"; // Данная реализацяи работает только на цифрах,
+        // сиволах и англиских буквах
+        OutputStream printStream = System.out;
+
+        write(out, printStream);
+
         byte[] info = new byte[12];
-        System.out.println("Ввведите текст, для записи");
         System.in.read(info);
-        char[] result = new char[12];
+
+        write("You entered: ", printStream);
 
         for (int i = 0; i < info.length; i++) {
-            result[i] = (char) info[i];
+            System.out.write(info[i]);
         }
+        printStream.close();
+    }
 
-        String resultString = new String(result);
-        System.out.println("Вы ввели:" + resultString);
+    private static void write(String out, OutputStream printStream) throws IOException {
+        char[] mas = out.toCharArray();
+
+        for (int i = 0; i < out.length(); i++) {
+            int a = mas[i];
+            printStream.write(a);
+        }
+        printStream.flush();
     }
 }
